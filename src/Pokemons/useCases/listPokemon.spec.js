6f769c20af30817990d8a7b35d085dec083bf296
Daniel <danieldeandradelopes@gmail.com>
2023-07-20 17:51:38 -0300

    fix: separete tests file

--- src/Pokemons/useCases/listPokemon.spec.js
+++ src/Pokemons/useCases/listPokemon.spec.js
@@ -1,6 +1,5 @@
 const listPokemons = require("./listPokemons");
 const inMemoryRepository = require("../repositories/inMemoryRepository");
-const insertPokemon = require("./insertPokemon");
 
 describe("Test listPokemon useCase", () => {
   test("Should be have a 10 pokemons in this list", async () => {
@@ -15,25 +14,5 @@ describe("Test listPokemon useCase", () => {
     const pikachu = pokemons.find((pokemon) => pokemon.name === "Pikachu");
 
     expect(pikachu.name).toBe("Pikachu");
-  });
-
-  test("Should be have pikachu 2 in list", async () => {
-    const insertedPokemon = await insertPokemon(inMemoryRepository, {
-      name: "Pikachu 2",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-      type: "Electric",
-    });
-
-    expect(insertedPokemon.name).toBe("Pikachu 2");
-  });
-
-  test("Should be have pikachu 3 in list", async () => {
-    const insertedPokemon = await insertPokemon(inMemoryRepository, {
-      name: "Pikachu 3",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-      type: "Electric",
-    });
-
-    expect(insertedPokemon.name).toBe("Pikachu 3");
   });
 });
